7fab10d5c82f2350a52b777337524282b5c62ba3
Robert Johnston <robert.johnston@capita.co.uk>
2017-08-30 15:31:19 +0100

    Filter

--- Code/LogbookApi/Models/FlightFilter.cs
+++ Code/LogbookApi/Models/FlightFilter.cs
@@ -1,6 +1,50 @@
-﻿namespace LogbookApi.Models
+﻿using System;
+
+namespace LogbookApi.Models
 {
+    [Flags]
+    public enum FilterType
+    {
+        None = 0,
+        Number = 1,
+        Date = 2,
+        Aircraft = 4,
+        Airfield = 8,
+        Launch = 16,
+        Crew = 32,
+        Trace = 64
+    }
     public class FlightFilter
     {
+        public FilterType FilterType { get; set; }
+
+        public int FlightStart { get; set; }
+
+        public int FlightEnd { get; set; }
+
+        public DateTime DateStart { get; set; }
+
+        public DateTime DateEnd { get; set; }
+
+        public int Aircraft { get; set; }
+
+        public int Airfield { get; set; }
+
+        public string Launch { get; set; }
+
+        public int Crew { get; set; }
+
+        public bool IsValid()
+        {
+            return FilterIsValid();
+        }
+
+        private bool FilterIsValid()
+        {
+            if(FilterType == FilterType.None) return false;
+            if(FilterType == (FilterType.Date & FilterType.Number)) return false;
+
+            return true;
+        }
     }
 }
