88994a79941a837d41e0c0ae6fe60205663cbfde
Robert Johnston <robert.johnston@capita.co.uk>
2017-09-14 08:58:01 +0100

    More Tests

--- Code/LogbookApi/Providers/Implementation/PageProvider.cs
+++ Code/LogbookApi/Providers/Implementation/PageProvider.cs
@@ -1,10 +1,18 @@
 ﻿using System.Threading.Tasks;
+using CuttingEdge.Conditions;
 using LogbookApi.Models;
 
 namespace LogbookApi.Providers.Implementation
 {
     public class PageProvider : IPageProvider
     {
+        private jetstrea_LogbookEntities _context;
+
+        public PageProvider(jetstrea_LogbookEntities context)
+        {
+            Condition.Requires(context, nameof(context)).IsNotNull();
+            _context = context;
+        }
         public Task<Page> GetPage(int page)
         {
             throw new System.NotImplementedException();
---
+++ Code/LogbookApiTest/Providers/PageProviderTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.Entity;
+using FluentAssertions;
+using LogbookApi;
+using LogbookApi.Providers;
+using LogbookApi.Providers.Implementation;
+using Moq;
+using NUnit.Framework;
+
+namespace LogbookApiTest.Providers
+{
+    [TestFixture()]
+    public class PageProviderTests
+    {
+        private Mock<jetstrea_LogbookEntities> Context { get; set; }
+
+        private Mock<DbSet<Flight>> FlightDbSet { get; set; }
+
+        private Mock<DbSet<Page>> PageDbSet { get; set; }
+
+        //private Mock<IEntityProvider<Airfield>> MockAirfieldProvider { get; set; }
+
+        //private Mock<IEntityProvider<Aircraft>> MockAircraftProvider { get; set; }
+
+        [SetUp]
+        public void SetupTests()
+        {
+            Context = new Mock<jetstrea_LogbookEntities>();
+            FlightDbSet = new Mock<DbSet<Flight>>();
+            PageDbSet = new Mock<DbSet<Page>>();
+
+            //MockAirfieldProvider = new Mock<IEntityProvider<Airfield>>();
+
+            //MockAircraftProvider = new Mock<IEntityProvider<Aircraft>>();
+        }
+        [Test]
+        public void ShouldReturnProvider()
+        {
+            var pp = GetTestSubject();
+            pp.Should().BeAssignableTo<IPageProvider>();
+        }
+
+        [Test]
+        public void ShouldThrowArgumentNullexceptionOnNullContext()
+        {
+            Action act = () => new PageProvider(null);
+
+            act.ShouldThrow<ArgumentNullException>().And.ParamName.Should().Be("context");
+        }
+
+        [Test]
+
+        public void ShouldReturnPageOnPageFound()
+        {
+            var pp = GetTestSubject();
+        }
+
+        private IPageProvider GetTestSubject()
+        {
+            return new PageProvider(Context.Object);
+        }
+    }
+}
